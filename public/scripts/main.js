4ab8316739a4ba4a0fae0201cf5816f7184cac0d
Luke Holman <LukeHolman@my.unt.edu>
2020-08-22 15:15:32 -0500

    Removed extra footer functions from main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -455,37 +455,6 @@ function resetLabelButtons() {
     unknown = false;
 }
 
-
-window.addEventListener("load", activateStickyFooter);
-
-function activateStickyFooter() {
-    adjustFooterCssTopToSticky();
-    window.addEventListener("resize", adjustFooterCssTopToSticky);
-}
-
-function adjustFooterCssTopToSticky() {
-    const footer = document.querySelector("#footer");
-    const bounding_box = footer.getBoundingClientRect();
-    const footer_height = bounding_box.height;
-    const window_height = window.innerHeight;
-    const above_footer_height = bounding_box.top - getCssTopAttribute(footer);
-    if (above_footer_height + footer_height <= window_height) {
-        const new_footer_top = window_height - (above_footer_height + footer_height);
-        footer.style.top = new_footer_top + "px";
-    } else if (above_footer_height + footer_height > window_height) {
-        footer.style.top = null;
-    }
-}
-
-function getCssTopAttribute(htmlElement) {
-    const top_string = htmlElement.style.top;
-    if (top_string === null || top_string.length === 0) {
-        return 0;
-    }
-    const extracted_top_pixels = top_string.substring(0, top_string.length - 2);
-    return parseFloat(extracted_top_pixels);
-}
-
 function left() {
 
 }
@@ -533,7 +502,6 @@ async function checkLabels() {
 function getVideo() {
     var rand_num = getRandomInt(0, storage_vids.length - 1)
     console.log(rand_num)
-    console.log(storage_vids);
     current_id = parseInt(storage_vids[rand_num]);
     var video = 'videos/' + storage_vids[rand_num].toString() + '.mp4';
     var storageRef = firebase.storage().ref();
